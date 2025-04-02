0764b94e01f751d43bc792cc3b5eaaf2b336aa98
Your Name <you@example.com>
2025-04-03 01:17:39 +0300

    sd

--- src/routes/gameRoutes.js
+++ src/routes/gameRoutes.js
@@ -3,6 +3,7 @@ const { handleGameConnection } = require('../controllers/gameController');
 const router = express.Router();
 
 module.exports = (app) => {
+    // Убедитесь, что express-ws инициализировано до использования app.ws
     app.ws('/game/:roomId', handleGameConnection);
     return router;
 };
